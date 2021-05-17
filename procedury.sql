3e194c3c672dbccc68bdd9e3d38dda15a5517074
Dominik <dominik.grudzien00@gmail.com>
2021-05-17 19:37:34 +0200

    Procedura pacjenci. Sory za logi

--- procedury.sql
+++ procedury.sql
@@ -136,6 +136,160 @@ END LOOP;
 END;
 
 /
+
+CREATE OR REPLACE PROCEDURE transformacja_pacjenci IS
+P_ID pacjenci.pacjent_id%type := 1;
+ID_A pacjenci.adres_id%type;
+ID_K pacjenci.kontakt_id%type;
+ID_MAX pacjenci.pacjent_id%TYPE;
+
+imie pacjenci.imie%TYPE;
+nazwisko pacjenci.nazwisko%TYPE;
+login pacjenci.login%TYPE;
+haslo pacjenci.haslo%TYPE;
+pesel pacjenci.pesel_id%TYPE;
+
+data_ur karty.data_ur%TYPE;
+grupa_krwi karty.grupa_krwi%TYPE;
+
+telefon kontakty.telefon%type;
+email kontakty.email%type;
+
+kod_poczt adresy.kod_poczt%type;
+miasto adresy.miasto%type;
+wojewodztwo adresy.wojewodztwo%type;
+ulica adresy.ulica%type;
+nrdomu adresy.nr_domu%type;
+nrmieszkania adresy.nr_mieszkania%type;
+
+CURSOR pobierz_max_id IS
+SELECT MAX(pacjent_id) FROM pacjenci;
+
+-- KARTY
+CURSOR pobierz_date_ur(pesel_pac karty.pesel_id%TYPE) IS
+SELECT data_ur FROM karty WHERE pesel_id = pesel_pac;
+
+CURSOR pobierz_grupe_krwi(pesel_pac karty.pesel_id%TYPE) IS
+SELECT grupa_krwi FROM karty WHERE pesel_id = pesel_pac;
+
+-- PACJENCI
+CURSOR pobierz_pesel(IDP IN NUMBER) IS
+SELECT pesel_id FROM pacjenci WHERE pacjent_id = IDP;
+
+CURSOR pobierz_ida(IDP IN NUMBER) IS SELECT
+p.adres_id FROM pacjenci p WHERE p.pacjent_id = IDP;
+
+CURSOR pobierz_idk(IDP IN NUMBER) IS SELECT
+p.kontakt_id FROM pacjenci p WHERE p.pacjent_id = IDP;
+
+CURSOR pobierz_imie(IDP IN NUMBER) IS
+SELECT imie FROM pacjenci WHERE pacjent_id = IDP;
+
+CURSOR pobierz_nazwisko(IDP IN NUMBER) IS
+SELECT nazwisko FROM pacjenci WHERE pacjent_id = IDP;
+
+CURSOR pobierz_login(IDP IN NUMBER) IS
+SELECT login FROM pacjenci WHERE pacjent_id = IDP;
+
+CURSOR pobierz_haslo(IDP IN NUMBER) IS
+SELECT haslo FROM pacjenci WHERE pacjent_id = IDP;
+
+-- KONTAKTY
+CURSOR pobierz_tel(IDK IN NUMBER) IS SELECT 
+k.telefon FROM kontakty k WHERE k.kontakt_id = IDK;
+
+CURSOR pobierz_email(IDK IN NUMBER) IS SELECT 
+k.email FROM kontakty k WHERE k.kontakt_id = IDK;
+
+-- ADRESY
+CURSOR pobierz_kod(IDA IN NUMBER) IS SELECT 
+a.kod_poczt FROM adresy a WHERE a.adres_id = IDA;
+CURSOR pobierz_miasto(IDA IN NUMBER) IS SELECT 
+a.miasto FROM adresy a WHERE a.adres_id = IDA;
+CURSOR pobierz_woj(IDA IN NUMBER) IS SELECT 
+a.wojewodztwo FROM adresy a WHERE a.adres_id = IDA;
+CURSOR pobierz_ul(IDA IN NUMBER) IS SELECT 
+a.ulica FROM adresy a WHERE a.adres_id = IDA;
+CURSOR pobierz_dom(IDA IN NUMBER) IS SELECT 
+a.nr_domu FROM adresy a WHERE a.adres_id = IDA;
+CURSOR pobierz_mieszk(IDA IN NUMBER) IS SELECT 
+a.nr_mieszkania FROM adresy a WHERE a.adres_id = IDA;
+
+BEGIN
+    OPEN pobierz_max_id;
+        FETCH pobierz_max_id INTO ID_MAX;
+    CLOSE pobierz_max_id;
+
+LOOP
+    OPEN pobierz_imie(P_ID);
+        FETCH pobierz_imie INTO imie;
+    CLOSE pobierz_imie;
+    
+    OPEN pobierz_nazwisko(P_ID);
+        FETCH pobierz_nazwisko INTO nazwisko;
+    CLOSE pobierz_nazwisko;
+    
+    OPEN pobierz_login(P_ID);
+        FETCH pobierz_login INTO login;
+    CLOSE pobierz_login;
+    
+    OPEN pobierz_haslo(P_ID);
+        FETCH pobierz_haslo INTO haslo;
+    CLOSE pobierz_haslo;
+    
+    OPEN pobierz_pesel(P_ID);
+        FETCH pobierz_pesel INTO pesel;
+    CLOSE pobierz_pesel;
+    
+    OPEN pobierz_date_ur(pesel);
+        FETCH pobierz_date_ur INTO data_ur;
+    CLOSE pobierz_date_ur;
+    
+    OPEN pobierz_grupe_krwi(pesel);
+        FETCH pobierz_grupe_krwi INTO grupa_krwi;
+    CLOSE pobierz_grupe_krwi;
+    
+    OPEN pobierz_ida(P_ID);
+        FETCH pobierz_ida INTO ID_A;
+    CLOSE pobierz_ida;
+    OPEN pobierz_idk(P_ID);
+        FETCH pobierz_idk INTO ID_K;
+    CLOSE pobierz_idk;
+    OPEN pobierz_tel(ID_K);
+        FETCH pobierz_tel INTO telefon;
+    CLOSE pobierz_tel;
+    OPEN pobierz_email(ID_K);
+        FETCH pobierz_email INTO email;
+    CLOSE pobierz_email;
+    OPEN pobierz_kod(ID_A);
+        FETCH pobierz_kod INTO kod_poczt;
+    CLOSE pobierz_kod;
+    OPEN pobierz_miasto(ID_A);
+        FETCH pobierz_miasto INTO miasto;
+    CLOSE pobierz_miasto;
+    OPEN pobierz_woj(ID_A);
+        FETCH pobierz_woj INTO wojewodztwo;
+    CLOSE pobierz_woj;
+    OPEN pobierz_ul(ID_A);
+        FETCH pobierz_ul INTO ulica;
+    CLOSE pobierz_ul;
+    OPEN pobierz_dom(ID_A);
+        FETCH pobierz_dom INTO nrdomu;
+    CLOSE pobierz_dom;
+    OPEN pobierz_mieszk(ID_A);
+        FETCH pobierz_mieszk INTO nrmieszkania;
+    CLOSE pobierz_mieszk;
+
+    INSERT INTO h_pacjenci VALUES(P_ID,imie,nazwisko,login,haslo,telefon,email,kod_poczt,miasto,wojewodztwo,ulica,nrdomu,nrmieszkania,pesel,data_ur,grupa_krwi);
+    
+    P_ID := P_ID + 1;
+    EXIT WHEN P_ID = ID_MAX;
+END LOOP;
+
+END;
+/
+
 --EXEC--ZONE--
 EXEC transformacja_placowki;
 EXEC transformacja_gabinety;
+EXEC transformacja_pacjenci;
